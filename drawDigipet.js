d521005ec59e24258b8edaa9100b2904872fe244
char55 <haras.charlotte@gmail.com>
2022-10-12 23:20:36 -0400

    some logic for pattern cycling

--- drawDigipet.js
+++ drawDigipet.js
@@ -1,27 +1,4 @@
-const pixelCanvas = document.querySelector('.pixel-canvas')
-
-function makeDigiPet(digiPattern) {
-  let gridHeight = 15
-  let gridWidth = 20
-  // If grid already present, clears any cells that have been filled in
-  while (pixelCanvas.firstChild) {
-    pixelCanvas.removeChild(pixelCanvas.firstChild)
-  }
-  // Creates rows and cells
-  for (let i = 1; i <= gridHeight; i++) {
-    let gridRow = document.createElement('tr')
-    pixelCanvas.appendChild(gridRow)
-    for (let j = 1; j <= gridWidth; j++) {
-      let gridCell = document.createElement('td')
-      if (digiPattern[i] && digiPattern[i].find(x => x == j)) {
-        console.log(i, j)
-        gridCell.style.backgroundColor = 'black'
-      }
-      gridRow.appendChild(gridCell)
-    }
-  }
-}
-
+// import { EGG_PATTERN, BABY_PATTERN } from './patterns';
 const baby = [
   [],
   [],
@@ -37,8 +14,8 @@ const baby = [
   [2, 4, 11],
   [2, 5, 6, 11],
   [2, 3, 10, 11],
-  [3, 4, 5, 6, 7, 8, 9, 10]
-]
+  [3, 4, 5, 6, 7, 8, 9, 10],
+];
 
 const baby2 = [
   [],
@@ -55,8 +32,8 @@ const baby2 = [
   [2, 11],
   [2, 5, 6, 11],
   [2, 3, 10, 11],
-  [3, 4, 5, 6, 7, 8, 9, 10]
-]
+  [3, 4, 5, 6, 7, 8, 9, 10],
+];
 
 const egg = [
   [],
@@ -73,8 +50,8 @@ const egg = [
   [2, 8, 6],
   [2, 8],
   [2, 3, 7, 8],
-  [3, 4, 5, 6, 7]
-]
+  [3, 4, 5, 6, 7],
+];
 
 const eggSquish = [
   [],
@@ -91,29 +68,66 @@ const eggSquish = [
   [2, 8, 6],
   [2, 8],
   [2, 3, 7, 8],
-  [3, 4, 5, 6, 7]
-]
+  [3, 4, 5, 6, 7],
+];
 
-makeDigiPet(baby)
+const EGG_PATTERN = [egg, eggSquish];
+const BABY_PATTERN = [baby, baby2];
 
-let squished
-//  if(squished){
-//     makeDigiPet(egg)
-//  } else{
-//     makeDigiPet(eggSquish)
-//  }
+const pixelCanvas = document.querySelector('.pixel-canvas');
+var form = 'egg'; // make into const
+var eggLove = 0;
+var index = 0;
+var delayInSeconds = 5;
 
-// while(true){
-//     setTimeout(() => { squished = !squished}, 5000);
-// }
+function makeDigiPet(digiPattern) {
+  let gridHeight = 15;
+  let gridWidth = 20;
+  // If grid already present, clears any cells that have been filled in
+  while (pixelCanvas.firstChild) {
+    pixelCanvas.removeChild(pixelCanvas.firstChild);
+  }
+  // Creates rows and cells
+  for (let i = 1; i <= gridHeight; i++) {
+    let gridRow = document.createElement('tr');
+    pixelCanvas.appendChild(gridRow);
+    for (let j = 1; j <= gridWidth; j++) {
+      let gridCell = document.createElement('td');
+      if (digiPattern[i] && digiPattern[i].find((x) => x == j)) {
+        // console.log(i, j);
+        gridCell.style.backgroundColor = 'black';
+      }
+      gridRow.appendChild(gridCell);
+    }
+  }
+}
+
+const getPattern = () => {
+  if (form == 'egg') {
+    return EGG_PATTERN;
+  }
+  if (form === 'baby') {
+    return BABY_PATTERN;
+  }
+};
 
 document.body.onclick = function () {
-  if (squished) {
-    makeDigiPet(baby)
-  } else {
-    makeDigiPet(baby2)
+  if (form === 'egg' && eggLove < 5) {
+    eggLove++;
+  } else if (form === 'egg' && eggLove >= 5) {
+    // hatch!
+    form = 'baby';
   }
-  squished = !squished
-  clearTimeout(initial)
-  // re-invoke invocation()
-}
+  // add heart motif
+};
+
+setInterval(() => {
+  const digiPattern = getPattern();
+
+  makeDigiPet(digiPattern[index]);
+  index++;
+  // index = index === totalPatterns ? 0 : index+1
+  if (index >= digiPattern.length) {
+    index = 0;
+  }
+}, delayInSeconds * 60);
